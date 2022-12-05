dc2f03052151eade5f6696b4e9351f353ff6d827
B.A Thisara Dilshan <88025945+THISARA005@users.noreply.github.com>
2022-12-05 16:31:02 +0530

    admin login succes

--- MarqueBeyond/admin/db/config.php
+++ MarqueBeyond/admin/db/config.php
@@ -5,7 +5,7 @@
     $passcode = '';
     $db = 'eauction';
     $conn = mysqli_connect($host , $user , $passcode , $db) or die('Connection Failed');
-    $hostname = 'http://localhost/MyFolder/MarqueBeyond/MarqueBeyond/admin';
+    $hostname = 'http://localhost/MyFolder/MarqueBeyond/MarqueBeyond/admin/';
     $mainhost = 'http://localhost/MyFolder/MarqueBeyond/MarqueBeyond/';
     $ADMIN_IMAGE_PATH = 'assets/images/';
     $LOGO_IMAGE_PATH = 'assets/images/logo.jpg';
