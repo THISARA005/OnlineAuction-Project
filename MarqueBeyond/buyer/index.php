6d29cc9aa2eaedf114245e627bed88ce318b3281
B.A Thisara Dilshan <88025945+THISARA005@users.noreply.github.com>
2022-12-06 23:43:49 +0530

    Error correct in index file in buyer

--- MarqueBeyond/buyer/index.php
+++ MarqueBeyond/buyer/index.php
@@ -1,9 +1,14 @@
 <?php 
+$host = 'localhost';
+$user = 'root';
+$password = '';
+$db = 'eauction';
+$conn = mysqli_connect($host , $user , $password , $db ) or die('Db Connection failed' . mysqli_connect_error());
 session_start();
 if(isset($_SESSION['bu_userid'])){
     header("Location: overview.php");}
     else{
-        header("Location: {$hostname}");
+        header("Location: http://localhost/MyFolder/MarqueBeyond/MarqueBeyond/");
     }
     mysqli_close($conn);
 ?>
